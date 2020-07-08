734c383e7e085c6afca400b55704f46bf1bb7c88
justinelai <justine.lai@gmail.com>
2020-07-08 11:03:01 -0400

    first commit for notes app, migration successful

--- practicesite/practicesite/asgi.py
+++ practicesite/practicesite/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for jldj project.
+ASGI config for practicesite project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'jldj.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'practicesite.settings')
 
 application = get_asgi_application()
